422b8f2a5f5f186a39d84d0a1bb122d4118a3e9e
Michael Wawra <michael@wawra.co.uk>
2015-12-06 16:27:08 +0000

    small fixes and logging output to diagnost APNS issues on server

--- notific/apns.go
+++ notific/apns.go
@@ -1,46 +1,48 @@
 package notific
 
 import (
-  "github.com/anachronistic/apns"
-  "log"
+	"github.com/anachronistic/apns"
+	"log"
 )
 
 // NO RETURN, expected to be run as a gofunc.
 // Generate an APNS message and send it to apple!
 func SendIOSNotification(endpoint *Endpoint, notification *Notification) {
-  log.Println("Sending a message to APNS for endpoint ", endpoint.Id)
+	log.Println("Sending a message to APNS for endpoint ", endpoint.Id)
 
-  message, err := decodeMessage(notification.Payload)
+	message, err := decodeMessage(notification.Payload)
 
-  if err != nil {
-    log.Println("Failed to decode notification message: ", err)
-    UpdateNotification(notification, ERROR)
-    return
-  }
+	if err != nil {
+		log.Println("Failed to decode notification message: ", err)
+		UpdateNotification(notification, ERROR)
+		return
+	}
 
-  payload := apns.NewPayload()
-  payload.Alert = message
-  payload.Badge = 1
-  payload.Sound = "bingbong.aiff"
+	payload := apns.NewPayload()
+	payload.Alert = message
+	payload.Badge = 1
+	payload.Sound = "bingbong.aiff"
 
-  pn := apns.NewPushNotification()
-  pn.DeviceToken = endpoint.DeviceToken
-  pn.AddPayload(payload)
+	pn := apns.NewPushNotification()
+	pn.DeviceToken = endpoint.DeviceToken
+	pn.AddPayload(payload)
 
-  client := apns.NewClient(*apnsHost, *apnsCert, *apnsKey)
+	log.Println("Sending to APNS Host: ", *apnsHost)
 
-  resp := client.Send(pn)
-  alert, _ := pn.PayloadString()
+	client := apns.NewClient(*apnsHost, *apnsCert, *apnsKey)
 
-  status := REJECTED
+	resp := client.Send(pn)
+	alert, _ := pn.PayloadString()
 
-  if resp.Success {
-    log.Println(notification.Id, " send OK.")
-    status = SENT
-  } else {
-    log.Println(notification.Id, " failed: ", alert, ", ", resp.Error)
-    status = ERROR
-  }
+	status := REJECTED
 
-  UpdateNotification(notification, status)
+	if resp.Success {
+		log.Println(notification.Id, " send OK.")
+		status = SENT
+	} else {
+		log.Println(notification.Id, " failed: ", alert, ", ", resp.Error)
+		status = ERROR
+	}
+
+	UpdateNotification(notification, status)
 }
